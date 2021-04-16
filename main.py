43ef492e64c458e6ae908f01c9c3d1581f8c376b
Disposuit <gavinwilliams1026@gmail.com>
2021-04-16 01:13:57 -0400

    Implemented DecisionTree no tuning

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 def preprocess(data_csv):
@@ -18,33 +19,47 @@ def preprocess(data_csv):
     return dfSample
 
 
-def kNN(training, validation, k):
+def kNN(training, validation, labelname, k):
     """Code for applying k-Nearest Neighbours algorithm"""
-    explicitLabel = training[['explicit']].to_numpy().reshape(len(training))
-    explicitsample = training.drop(['explicit'], axis=1)
-    neigh = KNeighborsClassifier(n_neighbors=k)
-    neigh.fit(explicitsample, explicitLabel)
-    explicitsample = validation.drop(['explicit'], axis=1)
-    predictions = pd.DataFrame(neigh.predict(explicitsample), columns=['explicit'])
-    accuracy = np.where(validation['explicit'].reset_index(drop=True) == predictions['explicit'], 1, 0)
-    return accuracy.sum()/len(accuracy), neigh
+    explicitLabel = training[[labelname]].to_numpy().reshape(len(training))
+    explicitsample = training.drop([labelname], axis=1)
+    knc = KNeighborsClassifier(n_neighbors=k)
+    knc.fit(explicitsample, explicitLabel)
+    explicitsample = validation.drop([labelname], axis=1)
+    predictions = pd.DataFrame(knc.predict(explicitsample), columns=[labelname])
+    accuracy = np.where(validation[labelname].reset_index(drop=True) == predictions[labelname], 1, 0)
+    return accuracy.sum()/len(accuracy), knc
+
+
+def decisionTree(training, validation, labelname):
+    explicitLabel = training[[labelname]].to_numpy().reshape(len(training))
+    explicitsample = training.drop([labelname], axis=1)
+    dtc = DecisionTreeClassifier()
+    dtc.fit(explicitsample, explicitLabel)
+    explicitsample = validation.drop([labelname], axis=1)
+    predictions = pd.DataFrame(dtc.predict(explicitsample), columns=[labelname])
+    accuracy = np.where(validation[labelname].reset_index(drop=True) == predictions[labelname], 1, 0)
+    return accuracy.sum() / len(accuracy), dtc
 
 
 data = "./archive/data.csv"
 cleanedData = preprocess(data)
 
-maxacc = 0
+knnmaxacc = 0
 bestknn = None
+dtcmaxacc = 0
+bestdtc = None
 nfolds = 10
 for i in range(0, 1000, 100):
     j = i+int(1000/nfolds)
     validationset = cleanedData.iloc[i:j]
     trainingset = cleanedData.drop(validationset.isin(cleanedData).index)
-    accuracy, knnmodel = kNN(trainingset, validationset, 10)
-    print(accuracy)
-    if accuracy > maxacc:
-        bestknn = maxacc
-"""
-modelabel = cleanedData[['mode']]
-modesample = cleanedData.drop(['mode'], axis=1)"""
+    accuracy, currmodel = kNN(trainingset, validationset, 'explicit', 10)
+    if accuracy > knnmaxacc:
+        knnmaxacc = accuracy
+        bestknn = currmodel
+    accuracy, currmodel = decisionTree(trainingset, validationset, 'mode')
+    if accuracy > dtcmaxacc:
+        dtcmaxacc = accuracy
+        bestdtc = currmodel
 
